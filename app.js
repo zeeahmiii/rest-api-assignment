a279438401414aeff057f9a83d7a82fee7684da5
zeeahmiii <101631915+zeeahmiii@users.noreply.github.com>
2022-06-02 01:47:28 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose=require("mongoose");
+var cors=require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/api/users');
@@ -21,6 +22,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use()
+app.use(cors);
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
