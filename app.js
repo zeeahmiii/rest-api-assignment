9a5dc632fa9f5dc77b78b0bc8e9184055d3c6025
zeeahmiii <101631915+zeeahmiii@users.noreply.github.com>
2022-06-04 19:09:08 +0500

    Update app.js

--- app.js
+++ app.js
@@ -48,9 +48,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
+
 
 mongoose
 .connect(db,
