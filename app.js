1922b17b6387c50bcd6446a9e5d3ba4e41ff2ae5
zeeahmiii <101631915+zeeahmiii@users.noreply.github.com>
2022-06-04 16:47:55 +0500

    Update app.js

--- app.js
+++ app.js
@@ -22,8 +22,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use()
-app.use(cors());
+//app.use()
+app.use(cors({origin:"*",}));
+
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
