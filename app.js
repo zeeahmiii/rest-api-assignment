18d0e7632e5b756201c44fabdfe862ee32cc2285
zeeahmiii <101631915+zeeahmiii@users.noreply.github.com>
2022-06-05 00:31:13 +0500

    Update app.js

--- app.js
+++ app.js
@@ -19,16 +19,16 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors({origin:"*",
+  methods:["GET","POST","PUT","DELETE"],
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use()
-app.use(cors({origin:"*",
-  methods:["GET","POST","PUT","DELETE"],
-}));
+
 
 
 app.use('/', indexRouter);
